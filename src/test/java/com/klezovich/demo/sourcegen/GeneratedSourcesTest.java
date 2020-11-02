adff9c5d14a400db76cf5121debc734e389839a1
aklezovics <arturs.klezovics@raisin.com>
2020-11-02 21:07:55 +0100

    Add class generation from JSON schema

--- src/test/java/com/klezovich/demo/sourcegen/GeneratedSourcesTest.java
+++ src/test/java/com/klezovich/demo/sourcegen/GeneratedSourcesTest.java
@@ -1,15 +1,25 @@
 package com.klezovich.demo.sourcegen;
 
+import com.example.types.ShippingAddress;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class GeneratedSourcesTest {
 
     @Test
-    public void testCanUseGeneratedSources() {
+    public void testCanUseGeneratedSourcesJaxb() {
         var factory = new ObjectFactory();
         assertNotNull(factory.createProduct());
         assertNotNull(factory.createCreateProductRequest());
+    }
+
+    @Test
+    public void testCanUseGeneratedSourcesJson() {
+        var addr = new ShippingAddress();
+        assertNotNull(addr);
+        addr.setCountryName("USA");
+        assertEquals("USA", addr.getCountryName());
     }
 }
