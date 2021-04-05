c78f682598374846c37a8fca680a7f0e211b588b
aklezovics <arturs.klezovics@raisin.com>
2021-04-05 19:56:45 +0200

    Add thymeleaf form example

--- src/main/java/com/klezovich/demo/controller/ThymeleaftController.java
+++ src/main/java/com/klezovich/demo/controller/ThymeleaftController.java
@@ -1,7 +1,10 @@
 package com.klezovich.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -27,4 +30,39 @@ public class ThymeleaftController {
 
         return new ModelAndView("hello_thymeleaf", "model", model);
     }
+
+    @GetMapping("/register")
+    public ModelAndView register() {
+        return new ModelAndView("register", "register_user", new RegisterUser());
+    }
+
+    @PostMapping("/do_registration")
+    public ModelAndView doRegister(@ModelAttribute("register_user") RegisterUser registerUser) {
+        System.out.println("User name:"+registerUser.getUsername());
+        System.out.println("Password:"+registerUser.getPassword());
+        return new ModelAndView("registration_ok","register_user",registerUser);
+    }
+
+    static class RegisterUser {
+        private String username;
+        private String password;
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
+
+
 }
